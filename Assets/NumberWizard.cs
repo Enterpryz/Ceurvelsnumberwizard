208b10961c2d85e0cb18c6fbc26158e9cdea5395
Enterpryz <enterpryz1@gmail.com>
2020-02-04 15:49:06 -0600

    Added text elements, beginning rest of coding.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to my realm, I am the Wizard of Numbers!");
+        Debug.Log("Ah, you wish to test my awesome powers? Choose a number, then!");
+        Debug.Log("Ah, I must add that the number simply MUST be between 1 and 1000! I can't count higher than that.");
+        Debug.Log("And don't forget to keep it a secret! It would be too easy if you told me!");          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
