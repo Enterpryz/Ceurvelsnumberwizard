6016a8fd3cfb42b547eabcc666e0a379a4cae2bd
Enterpryz <enterpryz1@gmail.com>
2020-02-06 14:42:37 -0600

    Added min, max coding. NumbWizard V. 1.0.1

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
         Debug.Log("Welcome to my realm, I am the Wizard of Numbers!");
         Debug.Log("Ah, you wish to test my awesome powers? Choose a number, then!");
-        Debug.Log("Ah, I must add that the number simply MUST be between 1 and 1000! I can't count higher than that.");
-        Debug.Log("And don't forget to keep it a secret! It would be too easy if you told me!");          
+        Debug.Log("Ah, I must add that the highest number you can choose is: " + max);
+        Debug.Log("And the smallest number that you can choose is: " + min);
+        Debug.Log("Don't forget to keep it a secret! It would be too easy if you told me!");          
     }
 
     // Update is called once per frame
