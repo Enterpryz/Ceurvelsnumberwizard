0f43eb341870fcf332018c15088a89d3ceb23342
Enterpryz <enterpryz1@gmail.com>
2020-02-06 15:03:46 -0600

    Added if keydown (key) code. V.1.0.2

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -13,12 +13,31 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Ah, you wish to test my awesome powers? Choose a number, then!");
         Debug.Log("Ah, I must add that the highest number you can choose is: " + max);
         Debug.Log("And the smallest number that you can choose is: " + min);
-        Debug.Log("Don't forget to keep it a secret! It would be too easy if you told me!");          
+        Debug.Log("Don't forget to keep it a secret! It would be too easy if you told me!");
+        Debug.Log("Is your number higher or lower than 500?");
+        Debug.Log("Press your up arrow if higher, and down arrow if lower.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
+
+
+
+
     }
+
 }
