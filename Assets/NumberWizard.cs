52050a8829bb8cfaad2d8025088a1e4615f96a6e
Enterpryz <enterpryz1@gmail.com>
2020-02-09 04:43:28 -0600

    Finished Base game, polished code slightly. Version pushed to V.1.1.0

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
 
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max;
+    int min;
+    int guess;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
 
+    }
 
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Note from Dev: Instructions are as follows:");
+        Debug.Log("Press your up arrow if higher, and down arrow if lower, Enter(or return) as correct.");
+        Debug.Log("...");
         Debug.Log("Welcome to my realm, I am the Wizard of Numbers!");
         Debug.Log("Ah, you wish to test my awesome powers? Choose a number, then!");
         Debug.Log("Ah, I must add that the highest number you can choose is: " + max);
         Debug.Log("And the smallest number that you can choose is: " + min);
         Debug.Log("Don't forget to keep it a secret! It would be too easy if you told me!");
         Debug.Log("Is your number higher or lower than 500?");
-        Debug.Log("Press your up arrow if higher, and down arrow if lower.");
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -28,26 +38,30 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
+
             min = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Down Arrow key was pressed.");
+
             max = guess;
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Return key was pressed.");
+            Debug.Log("Behold! I truly am the wisest of them all!");
+            StartGame();
         }
-
-
-
-
     }
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Is it higher or lower than " + guess);
+    }
+
+
 
 }
